47a7f3485abc6dbce6352f240e2137309fcd59f4
Viktor Kassov <kassov@mail.ua>
2021-11-29 12:23:40 +0200

    last stage

--- Portfolio/task/src/index.js
+++ Portfolio/task/src/index.js
@@ -2,6 +2,8 @@ let flashCardBtn = document.querySelector("#portfolio>.flashCard>button");
 let virtualPianoBtn = document.querySelector("#portfolio>.virtualPiano>button");
 let popUpWnd = document.querySelector("#popUpWindow");
 let closeBtn = popUpWnd.querySelector("button");
+let mobileNav = document.querySelector(".hamburger");
+let navItems = document.querySelector(".hamburgerItems");
 flashCardBtn.addEventListener("click", function () {
     popUpWnd.style.zIndex = '10';
 });
@@ -11,3 +13,14 @@ virtualPianoBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
     popUpWnd.style.zIndex = '-1';
 })
+mobileNav.addEventListener("click", function () {
+    if (navItems.style.visibility == "visible") {
+    	navItems.style.visibility = "hidden";
+    } else {
+    	navItems.style.visibility = "visible";
+    }    
+});
+
+navItems.querySelector("a").addEventListener("click", function () {
+    navItems.style.visibility = "hidden";   
+});
